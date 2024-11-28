0e3b783f14904394289117ceef6f7ef97cc83a2b
chillz-the-first <105022195+chillz-the-first@users.noreply.github.com>
2024-11-28 21:03:31 +0200

    Completed exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,25 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let sum = 0;
 
+    if (num1 < 0 || num2 < 0 || Number.isNaN(num1) || Number.isNaN(num2) || !Number.isInteger(num1) ||
+        !Number.isInteger(num2)) return "ERROR";
+
+    if (num1 > num2) {
+        for (let i = num1; i >= num2; i--) {
+            sum = sum + i;
+        }
+        return sum;
+    }
+    else {
+        for (let i = num1; i <= num2; i++) {
+            sum = sum + i;
+        }
+    }
+
+    return sum;
 };
+
+console.log(sumAll(123, 1));
 
 // Do not edit below this line
 module.exports = sumAll;
