76276210f34f123d90de551aef865e42a6fa7150
chillz-the-first <105022195+chillz-the-first@users.noreply.github.com>
2024-11-28 21:36:31 +0200

    Completed exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  let convertedTemp = (fahrenheit - 32) * (5 / 9);
+  convertedTemp = parseFloat(convertedTemp.toFixed(1));
+  if (convertedTemp === 0) return 0;
+  return convertedTemp;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  let convertedTemp = (celsius * (9 / 5)) + 32;
+  convertedTemp = parseFloat(convertedTemp.toFixed(1));
+  if (convertedTemp === 0) return 0;
+  return convertedTemp;
 };
 
 // Do not edit below this line
