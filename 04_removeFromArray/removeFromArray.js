519374c2ed8fcd6be3997f7ba2f8607f68535c75
chillz-the-first <105022195+chillz-the-first@users.noreply.github.com>
2024-11-28 20:16:38 +0200

    Removed unnecessary code

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,20 +1,10 @@
 const removeFromArray = function(array, element, ...rest) {
     let arr = [];
-
-    if (rest.length !== 0) {
-        rest.push(element);
-        rest.sort();
-        for (let i = 0; i < array.length; i++) {
-            if (!rest.includes(array[i])) {
-                arr.push(array[i]);
-            }
-        }
-    }
-    else {
-        for (let i = 0; i < array.length; i++) {
-            if (array[i] !== element) {
-                arr.push(array[i]);
-            }
+    rest.push(element);
+    rest.sort();
+    for (let i = 0; i < array.length; i++) {
+        if (!rest.includes(array[i])) {
+            arr.push(array[i]);
         }
     }
 
