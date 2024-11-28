c705b56e02902e91dfd9cfa32989d95c72da7c55
chillz-the-first <105022195+chillz-the-first@users.noreply.github.com>
2024-11-28 20:12:35 +0200

    Completed first four exercises

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let reverseString = "";
+    let size = str.length;
+    for (let i = size - 1; i >= 0; i--) {
+        reverseString += str.at(i);
+    }
+    return reverseString;
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,24 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, element, ...rest) {
+    let arr = [];
+
+    if (rest.length !== 0) {
+        rest.push(element);
+        rest.sort();
+        for (let i = 0; i < array.length; i++) {
+            if (!rest.includes(array[i])) {
+                arr.push(array[i]);
+            }
+        }
+    }
+    else {
+        for (let i = 0; i < array.length; i++) {
+            if (array[i] !== element) {
+                arr.push(array[i]);
+            }
+        }
+    }
+
+    return arr;
 };
 
 // Do not edit below this line
